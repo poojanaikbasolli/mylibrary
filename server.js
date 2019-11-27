cb82d42c0d06745b5600ed6f6e493cf766d2f407
pooja <poojanaikkodigar@gmail.com>
2019-11-27 13:46:10 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express')
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
   const mongoose = require('mongoose')
+  const bodyParser = require('body-parser')
+
   const indexRouter = require('./routes/index')
-  
+  const authorRouter = require('./routes/authors')
+
+
   app.set('view engine', 'ejs')
   app.set('views', __dirname + '/views')
   app.set('layout', 'layouts/layout')
   app.use(expressLayouts)
   app.use(express.static('public'))
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
   
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -22,5 +27,7 @@ mongoose
   .catch(err => console.log(err));
   
   app.use('/', indexRouter)
+  app.use('/authors', authorRouter)
+
   
   app.listen(process.env.PORT || 3000)
