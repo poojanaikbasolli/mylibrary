365dd2c9783fe3d70a5bd2854173a7e9a94d973d
pooja <poojanaikkodigar@gmail.com>
2019-11-29 12:34:42 +0530

    Author Routes Setup

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express')
   const expressLayouts = require('express-ejs-layouts')
   const mongoose = require('mongoose')
   const bodyParser = require('body-parser')
-
+  const methodOverride = require('method-override')
+  
   const indexRouter = require('./routes/index')
   const authorRouter = require('./routes/authors')
   const bookRouter = require('./routes/books')
@@ -13,6 +14,7 @@ const express = require('express')
   app.set('views', __dirname + '/views')
   app.set('layout', 'layouts/layout')
   app.use(expressLayouts)
+  app.use(methodOverride('_method'))
   app.use(express.static('public'))
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
   
